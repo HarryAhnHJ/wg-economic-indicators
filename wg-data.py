4c229be787c9623da1040d208d7dae316132bdd4
harryahnhj <harryahnhj@gmail.com>
2022-12-22 14:28:46 -0800

    todo add inf pct change

--- wg-data.py
+++ wg-data.py
@@ -53,27 +53,64 @@ def getCANData():
     sc = StatsCan()
     dfs = []
 
-    inf_raw = sc.table_to_df("18-10-0259-01")
-    inf_df = pd.DataFrame(data=inf_raw)
-    inf_df = inf_df[~inf_df["GEO"].str.contains("Canada|British Columbia") == False]
-    dfs.append(inf_df)
-    print(inf_df)
+    # inf_raw = sc.table_to_df("18-10-0259-01")
+    # inf_df = pd.DataFrame(data=inf_raw)
+    # print(inf_df.columns)
+    # inf_df = inf_df.drop_duplicates()
+    # inf_df = inf_df[~inf_df["GEO"].str.contains("Canada|British Columbia") == False]
+    # keeping rows with CPI COMMON ONLY 
+    # inf_df = inf_df[~inf_df["Alternative measures"].str.contains("CPI-common") == False]
+    # inf_df = inf_df[['REF_DATE','GEO','Alternative measures','VALUE']]
+
+    # dfs.append(inf_df)
+    # print(inf_df)
+   
+    # exportData(inf_df, "inflation")
 
     unemp_raw = sc.table_to_df("14-10-0287-03")
+    print(unemp_raw)
     unemp_df = pd.DataFrame(data=unemp_raw)
     unemp_df = unemp_df[~unemp_df["GEO"].str.contains("Canada|British Columbia") == False]
+    unemp_df = unemp_df.dropna()
     dfs.append(unemp_df)
-    print(unemp_df)
+  
+    exportData(unemp_df, "unemployment")
 
     cpi_df = ft.reduce(lambda left, right: pd.merge(left,right,how='outer',on='REF_DATE'),dfs)
     ax = cpi_df['REF_DATE','VALUEE'].plot(kind="line",grid=True)
     ax.text(0.5,-1, """Tracking inflation changes over time.""")
     plt.show()
 
+def getCSVData():
+
+    dfs = []
+    cpi_df = pd.read_csv(os.getcwd() + '\\data\\CPI.csv')
+    nhpi_df = pd.read_csv(os.getcwd() + '\\data\\New Housing Price Index.csv')
+
+
+    cpi_df = cpi_df.melt('REF_DATE', var_name = 'GEO',value_name = 'CPI')
+    # cpi_df['REF_DATE'] = pd.to_datetime(cpi_df.index.astype(str),format='%Y-%M-%D]')
+    print(cpi_df)
+    dfs.append(cpi_df)
+
+    nhpi_df = nhpi_df.replace('Total', 'Land & House', regex=True)
+    nhpi_df = nhpi_df.replace('Vancouver, British Columbia', 'Vancouver', regex=True)
+    nhpi_df = nhpi_df.replace('British Columbia 6', 'British Columbia', regex=True)
+    # nhpi_df['REF_DATE'] = pd.to_datetime(nhpi_df.index.astype(str),format='%Y-%M-%D')
+    print(nhpi_df)
+    dfs.append(nhpi_df)
+
+    cpi_df = ft.reduce(lambda left, right: pd.merge(left,right,how='outer',on=['REF_DATE','GEO']),dfs)
+    cpi_df = cpi_df[~cpi_df["GEO"].str.contains("Vancouver") == False]
+    exportData(cpi_df,"inflation")
+    ax = cpi_df.plot(x = 'REF_DATE',kind="line",grid=True)
+    ax.text(0.5,-1, """Tracking inflation changes over time.""")
+    plt.show()
 
 def getData():
-    getNASDAQData()
-    getCANData()
+    # getNASDAQData()
+    # getCANData()
+    getCSVData()
 
 def exportData(data,name):
         with pd.ExcelWriter('Economic Indicator - Cleaned.xlsx') as writer:
@@ -81,4 +118,4 @@ def exportData(data,name):
 
 if __name__ == "__main__":
     getData()
- 
+    #testing commit
